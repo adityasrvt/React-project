6e6498c391a555a71fe78858439055cfa5f56e68
adityasrvt <adityasri0804@gmail.com>
2026-02-07 19:25:32 +0530

    upd

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/My-Projects-/">
+    <BrowserRouter basename="/React-project/">
+
       <AuthProvider>
         <CartProvider>
           <App />
